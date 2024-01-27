736a556ef6d0e1977cb85358811e0b129d16d3e7
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-01-27 19:09:36 +0900

    Support the repository cloned by https
    internal/gitdir is clone of hymkor/make-scoop-manifest/internal/gitdir

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"bufio"
-	"errors"
 	"fmt"
-	"io"
 	"os"
-	"os/exec"
-	"regexp"
-	"strings"
+
+	"github.com/hymkor/make-howto-insta11/internal/gitdir"
 )
 
 const theTemplate = `[![GoDev](https://pkg.go.dev/badge/github.com/%[2]s/%[3]s)](https://pkg.go.dev/github.com/%[2]s/%[3]s)
@@ -34,61 +30,8 @@ scoop install %[4]s
 %[1]s
 `
 
-func quote(args []string, f func(string) error) error {
-	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Stdin = os.Stdin
-	cmd.Stderr = os.Stderr
-	r, err := cmd.StdoutPipe()
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-	cmd.Start()
-
-	sc := bufio.NewScanner(r)
-	for sc.Scan() {
-		//println(sc.Text())
-		if err := f(sc.Text()); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func listUpRemoteBranch() ([]string, error) {
-	branches := []string{}
-	quote([]string{"git", "remote", "show"}, func(line string) error {
-		branches = append(branches, strings.TrimSpace(line))
-		return nil
-	})
-	return branches, nil
-}
-
-var rxURL = regexp.MustCompile(`Push +URL: \w+@github.com:([\w\.-]+)/([\w\.-]+).git`)
-
-func getNameAndRepo() (string, string, error) {
-	branch, err := listUpRemoteBranch()
-	if err != nil {
-		return "", "", err
-	}
-	if len(branch) < 1 {
-		return "", "", errors.New("remote branch not found")
-	}
-	var user, repo string
-	quote([]string{"git", "remote", "show", "-n", branch[0]}, func(line string) error {
-		m := rxURL.FindStringSubmatch(line)
-		if m != nil {
-			user = m[1]
-			repo = m[2]
-			return io.EOF
-		}
-		return nil
-	})
-	return user, repo, nil
-}
-
 func mains() error {
-	user, repo, err := getNameAndRepo()
+	user, repo, err := gitdir.GetNameAndRepo()
 	if err != nil {
 		return err
 	}
